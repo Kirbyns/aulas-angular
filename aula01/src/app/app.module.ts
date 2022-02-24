75c07a1bc8180d002fed35cf4a6ffc7d365bbed6
matheus Oliveira <moasaraujo@gmail.com>
2022-02-24 19:08:44 -0300

    aula de angular

---
+++ aula01/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { Teste1Component } from './views/teste1/teste1.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Teste1Component
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
