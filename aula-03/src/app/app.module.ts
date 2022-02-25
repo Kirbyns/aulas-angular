9c980c04f1d09147116a67407ae927f354985788
matheus Oliveira <moasaraujo@gmail.com>
2022-02-24 22:25:17 -0300

    aula 03 finalizada com todos exemplos usados

--- aula-03/src/app/app.module.ts
+++ aula-03/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InterpolationComponent } from './views/interpolation/interpolation.component';
+import { Exercicio01Component } from './views/exercicio01/exercicio01.component';
+import { Exercicio02Component } from './views/exercicio02/exercicio02.component';
+import { Exercicio03Component } from './views/exercicio03/exercicio03.component';
+import { Exercicio04Component } from './views/exercicio04/exercicio04.component';
+import { EventBindingComponent } from './views/event-binding/event-binding.component';
+import { Exercicio05Component } from './views/exercicio05/exercicio05.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InterpolationComponent,
+    Exercicio01Component,
+    Exercicio02Component,
+    Exercicio03Component,
+    Exercicio04Component,
+    EventBindingComponent,
+    Exercicio05Component
   ],
   imports: [
     BrowserModule,
