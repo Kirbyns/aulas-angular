6fc61ad44374ba876f7026897796c58b6775d858
matheus Oliveira <moasaraujo@gmail.com>
2022-03-18 16:39:29 -0300

    praticando

--- pseudoProva/src/app/app.module.ts
+++ pseudoProva/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { Exercicio01Component } from './views/exercicio01/exercicio01.component';
 import {FormsModule} from '@angular/forms';
 import { Exercicio02Component } from './views/exercicio02/exercicio02.component';
+import { Exercicio03Component } from './views/exercicio03/exercicio03.component';
+import { Exercicio04Component } from './views/exercicio04/exercicio04.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Exercicio01Component,
-    Exercicio02Component
+    Exercicio02Component,
+    Exercicio03Component,
+    Exercicio04Component
   ],
   imports: [
     BrowserModule,
---
+++ pseudoProva/src/app/views/exercicio03/exercicio03.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exercicio03',
+  templateUrl: './exercicio03.component.html',
+  styleUrls: ['./exercicio03.component.css']
+})
+export class Exercicio03Component implements OnInit {
+
+
+
+  cor='';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
