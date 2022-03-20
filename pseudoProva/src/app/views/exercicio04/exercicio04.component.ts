e132a3a90d81a34baedba5ab14f8cd8d7229aa61
Matheus Oliveira <moasaraujo@gmail.com>
2022-03-19 23:57:59 -0300

    praticando

--- pseudoProva/src/app/views/exercicio04/exercicio04.component.ts
+++ pseudoProva/src/app/views/exercicio04/exercicio04.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exercicio04.component.css']
 })
 export class Exercicio04Component implements OnInit {
+  imagem ='imagem01';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  mudar(){
+    if(this.imagem =="imagem01"){
+      this.imagem ="imagem02" ;
+
+    } else if(this.imagem =="imagem02"){
+      this.imagem ="imagem03";
+
+    }else{
+      this.imagem ="imagem01";
+    }
+
+
+
   }
 
 }
